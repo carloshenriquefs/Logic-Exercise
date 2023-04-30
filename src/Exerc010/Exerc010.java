48b994c952e9e3d4d83647a05c45686b73e1ce7d
Carlos Henrique <ichfs9108@gmail.com>
2023-04-30 03:24:28 -0300

    Exerc-010-sumAverage

---
+++ src/Exerc010/Exerc010.java
@@ -0,0 +1,42 @@
+package Exerc011;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc11 {
+    //nelio
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos números você vai digitar ? ");
+        int number = sc.nextInt();
+
+        Double vector[] = new Double[number];
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("Digite um número: ");
+            vector[i] = sc.nextDouble();
+        }
+
+        System.out.print("VALORES = ");
+        for (int i = 0; i < number; i++) {
+            System.out.print(vector[i] + "  ");
+        }
+
+        Double soma = 0.0;
+        for (int i = 0; i < number; i++) {
+            soma += vector[i];
+        }
+
+        Double media = soma / number;
+
+        System.out.println();
+        System.out.print("SOMA = " + soma);
+        System.out.println();
+        System.out.print("MEDIA = " + media);
+
+        sc.close();
+    }
+}
