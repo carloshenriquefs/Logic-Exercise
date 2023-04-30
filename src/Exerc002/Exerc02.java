e041a7181d517930e66c979c68e92eb0e7386b23
Carlos Henrique <ichfs9108@gmail.com>
2023-04-30 02:40:52 -0300

    Exerc-002-numbers

---
+++ src/Exerc002/Exerc02.java
@@ -0,0 +1,23 @@
+package Exerc002;
+
+import java.util.Scanner;
+
+public class Exerc02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numberFirst = sc.nextInt();
+        int numberSecond = sc.nextInt();
+        int numberThird = sc.nextInt();
+
+        System.out.println();
+        System.out.println(numberFirst);
+        System.out.println(numberSecond);
+        System.out.println(numberThird);
+
+        sc.close();
+    }
+
+}
