148f4c60fd66769badc31d184c668b37a6558bb6
Carlos Henrique <ichfs9108@gmail.com>
2023-04-30 02:54:43 -0300

    Exerc-005-StringInt

---
+++ src/Exerc005/Exerc05.java
@@ -0,0 +1,19 @@
+package Exerc005;
+
+import java.util.Scanner;
+
+public class Exerc05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===========================");
+        for(int i = 0; i< 3; i++) {
+            String s1 = sc.next();
+            Integer x = sc.nextInt();
+
+        }
+        System.out.println("==========================");
+
+        sc.close();
+    }
+}
