069bc56687d6a07c84521f3007d9c5b3ba718f70
Carlos Henrique <ichfs9108@gmail.com>
2023-04-30 16:36:38 -0300

    Exerc-022-positionMatrix

---
+++ src/Exerc022/Exerc022.java
@@ -0,0 +1,44 @@
+package Exerc023;
+
+import java.util.Scanner;
+
+public class Exerc023 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int number = sc.nextInt();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == number) {
+                    System.out.println("Position " + i + "," + j + ": ");
+                    if (j > 0) {
+                        System.out.println("Left: " + matrix[i][j - 1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + matrix[i - 1][j]);
+                    }
+                    if (j < matrix[i].length - 1) {
+                        System.out.println("Right: " + matrix[i][j + 1]);
+                    }
+                    if (i < matrix.length - 1) {
+                        System.out.println("Down: " + matrix[i + 1][j]);
+                    }
+                }
+            }
+        }
+        sc.close();
+    }
+}
+
