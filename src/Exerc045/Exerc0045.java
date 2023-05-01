f51f9bb15affe7ec07b6d1989719e64977fd9a9b
Carlos Henrique <ichfs9108@gmail.com>
2023-04-30 22:55:08 -0300

    Exerc-045-messageOrder

---
+++ src/Exerc045/Exerc0045.java
@@ -0,0 +1,16 @@
+package Exerc046;
+
+import java.util.Scanner;
+
+public class Exerc0046 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; sc.hasNext(); i++) {
+            System.out.println(i + " " + sc.nextLine());
+        }
+
+        sc.close();
+    }
+}
---
+++ src/Exerc045/Exerc045.java
@@ -0,0 +1,26 @@
+package Exerc046;
+
+import java.util.Scanner;
+
+public class Exerc046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        var phaseOne = "Hello World";
+        var phaseSecond = "I am a file";
+        var phaseThird = "Read me until end-of-file";
+
+        for (int i = 1; i <= 1; i++) {
+            System.out.println(i + " " + phaseOne);
+
+        }
+        for (int i = 2; i <= 2; i++) {
+            System.out.println(i + " " + phaseSecond);
+        }
+        for (int i = 3; i <= 3; i++) {
+            System.out.println(i + " " + phaseThird);
+        }
+
+        sc.close();
+    }
+}
